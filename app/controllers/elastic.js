fb2447d298e04b3bf02e45b3b2e7f76774ea7b3d
Alexandre JARNOUX <alexandre.jarnoux@beesearch.fr>
2014-05-26 16:39:04 +0200

    Elasticsearch search setup

--- app/controllers/elastic.js
+++ app/controllers/elastic.js
@@ -1,27 +1,27 @@
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+  host: 'vps67962.ovh.net:9200',
+  log: 'trace'
+});
 
 exports.search = function(req, res) {
-	console.log('in search');
+  console.log('in search');
 
-	var search = req.query.search;
-	var page = parseInt(req.query.page) | 1;
-	var length = parseInt(req.query.length) | 10;
-	console.log('search: ' + search);
-	//console.log('page: ' + page);
-	//console.log('length: ' + length);
+  var search = req.query.search;
+  console.log('search: ' + search);
 
-	// Calculating start and end elements
-	var start = (page - 1) * length;
-	var end = (start + length) - 1;
-
-	var result = [];
-	for (var i = start; i <= end; i++) {
-		result.push({'id': i, 'text': 'Result number: ' + i, 'now': new Date().toJSON()});
-	};
-
-	//console.log('result: ' + JSON.stringify(result));
-	res.send(result);
+  client.search({
+    q: search
+  }).then(function (body) {
+    var hits = body.hits.hits;
+    console.log(hits);
+    res.send(hits);
+  }, function (error) {
+    console.trace(error.message);
+  });
 }
 
 exports.data = function(req, res) {
-	res.send({'type': 'Hello', 'now': new Date().toJSON()});
+  res.send({'type': 'Hello', 'now': new Date().toJSON()});
 }
