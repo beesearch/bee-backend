b4cfe404d8e8ff46ffa1d7a894dc9316fa37a8e4
robbee <alexandre.bonte@beesearch.fr>
2014-06-30 11:57:35 +0200

    unmock the ES facet access

--- app/controllers/model.js
+++ app/controllers/model.js
@@ -52,6 +52,7 @@ function getCompanyDatas(type, index, id, callback) {
 	});
 }
 
+
 function getCompanyTop5ProductsChart(type, index, id, callback) {
 	console.log('#### in getCompanyTop5ProductsChart (type: ' + type + ', index: ' + index  + ' , id: ' + id + ')');
 	esclient.search({
@@ -62,14 +63,17 @@ function getCompanyTop5ProductsChart(type, index, id, callback) {
 			query: {
 				term : { "customer.company.id" : id }
 			},
-			facets: {
-				tag : {
-					terms : {
-						field : "productId",
-						size : 5
-					}
-				}
-			}
+			  facets: {
+			    terms: {
+			      terms_stats: {
+			        key_field: "productCategory",
+			        value_field: "count",
+			        size: 0
+					},
+			       nested: "orders"
+			    }
+			  }
+
 		}
 	}).then(function (response) {
 		response = {"took":3,"timed_out":false,"_shards":{"total":5,"successful":5,"failed":0},"hits":{"total":100,"max_score":0,"hits":[]},"facets":{"terms":{"_type":"terms_stats","missing":0,"terms":[{"term":"boulon","count":832,"total_count":832,"min":100,"max":400,"total":212900,"mean":255.88942307692307},{"term":"ecrou","count":819,"total_count":819,"min":100,"max":400,"total":199100,"mean":243.1013431013431},{"term":"vis","count":776,"total_count":776,"min":100,"max":400,"total":196400,"mean":253.09278350515464},{"term":"joint","count":414,"total_count":414,"min":100,"max":400,"total":106100,"mean":256.280193236715},{"term":"rondelle","count":402,"total_count":402,"min":100,"max":400,"total":103900,"mean":258.4577114427861},{"term":"split","count":401,"total_count":401,"min":100,"max":400,"total":101500,"mean":253.11720698254365}]}}};
