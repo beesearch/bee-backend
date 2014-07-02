db7aa0bf0ce6f540e81855bd72ba43e65adf572a
Alexandre JARNOUX <alexandre.jarnoux@beesearch.fr>
2014-07-02 15:02:34 +0000

    Using _id

--- app/controllers/model.js
+++ app/controllers/model.js
@@ -36,18 +36,13 @@ function getCompany(type, index, id, res) {
 
 function getCompanyDatas(type, index, id, callback) {
 	console.log('#### in getCompanyDatas (type: ' + type + ', index: ' + index  + ' , id: ' + id + ')');
-	esclient.search({
+	esclient.get({
 		index: index,
 		type: type,
-		size: 1,
-		body: {
-			query: {
-				term : { "customer.companyId" : id }
-			}
-		}
+		id: id
 	}).then(function (response) {
 		// Callback with datas
-		callback(null, response.hits.hits[0]);
+		callback(null, response);
 	}, function (error) {
 		console.log(error.message);
 		callback(error);
@@ -62,7 +57,7 @@ function getCompanyTop5ProductsChart(type, index, id, callback) {
 		size: 0,
 		body: {
 			query: {
-				term : { "customer.companyId" : id }
+				term : { "_id" : id }
 			},
 			facets: {
 			    terms: {
@@ -73,10 +68,8 @@ function getCompanyTop5ProductsChart(type, index, id, callback) {
 			       nested: "orders"
 			    }
 			}
-
 		}
 	}).then(function (response) {
-		// response = {"took":3,"timed_out":false,"_shards":{"total":5,"successful":5,"failed":0},"hits":{"total":100,"max_score":0,"hits":[]},"facets":{"terms":{"_type":"terms_stats","missing":0,"terms":[{"term":"boulon","count":832,"total_count":832,"min":100,"max":400,"total":212900,"mean":255.88942307692307},{"term":"ecrou","count":819,"total_count":819,"min":100,"max":400,"total":199100,"mean":243.1013431013431},{"term":"vis","count":776,"total_count":776,"min":100,"max":400,"total":196400,"mean":253.09278350515464},{"term":"joint","count":414,"total_count":414,"min":100,"max":400,"total":106100,"mean":256.280193236715},{"term":"rondelle","count":402,"total_count":402,"min":100,"max":400,"total":103900,"mean":258.4577114427861},{"term":"split","count":401,"total_count":401,"min":100,"max":400,"total":101500,"mean":253.11720698254365}]}}};
 		// Transform ES response to chart
 		var chart = fromFacetsToChart(response.facets);
 		// Callback with chart
@@ -95,7 +88,7 @@ function getCompanyTop5SalesChart(type, index, id, callback) {
 		size: 0,
 		body: {
 			query: {
-				term : { "customer.companyId" : id }
+				term : { "_id" : id }
 			},
 			facets: {
 			    terms: {
@@ -106,10 +99,8 @@ function getCompanyTop5SalesChart(type, index, id, callback) {
 			       nested: "orders"
 			    }
 			}
-
 		}
 	}).then(function (response) {
-		// response = {"took":3,"timed_out":false,"_shards":{"total":5,"successful":5,"failed":0},"hits":{"total":100,"max_score":0,"hits":[]},"facets":{"terms":{"_type":"terms_stats","missing":0,"terms":[{"term":"boulon","count":832,"total_count":832,"min":100,"max":400,"total":212900,"mean":255.88942307692307},{"term":"ecrou","count":819,"total_count":819,"min":100,"max":400,"total":199100,"mean":243.1013431013431},{"term":"vis","count":776,"total_count":776,"min":100,"max":400,"total":196400,"mean":253.09278350515464},{"term":"joint","count":414,"total_count":414,"min":100,"max":400,"total":106100,"mean":256.280193236715},{"term":"rondelle","count":402,"total_count":402,"min":100,"max":400,"total":103900,"mean":258.4577114427861},{"term":"split","count":401,"total_count":401,"min":100,"max":400,"total":101500,"mean":253.11720698254365}]}}};
 		// Transform ES response to chart
 		var chart = fromFacetsToChart(response.facets);
 		// Callback with chart
