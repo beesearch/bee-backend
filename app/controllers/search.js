f1ea0a26dd88e4179e2ad81c2cf5da421c89adb3
Alexandre JARNOUX <alexandre.jarnoux@beesearch.fr>
2014-07-03 14:07:54 +0000

    Using indexes and types send by the client

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -4,22 +4,24 @@ var esclient = require('../../server').esclient;
 exports.companySearch = function(req, res) {
   console.log('### in companySearch');
 
-  var subsidiary = req.query.subsidiary
+  var indexes = req.query.indexes;
+  var types = req.query.types;
   var search = req.query.search;
-  console.log('#### subsidiary: ' + req.query.subsidiary);
-  console.log('#### search: ' + req.query.search);
+  console.log('#### indexes: ' + indexes);
+  console.log('####   types: ' + types);
+  console.log('####  search: ' + req.query.search);
 
   esclient.search({
-    index: subsidiary,
-    //type: 'customer',
+    index: indexes,
+    type: types,
     body: 
     {
       "fields" : ["company.name", "company.siren"],
       "query": {
         "match_phrase_prefix": {
           "company.name": {
-                "query": search,
-                "max_expansions": 5
+            "query": search,
+            "max_expansions": 5
           }
         }
       }
